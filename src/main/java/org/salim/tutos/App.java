3cf8f06bb75d992c63a9c8e1f8473e394b9ce530
Ghbabra Salim <salim.ghbabra@etu.univ-orleans.fr>
2020-06-23 16:15:47 +0200

    Jour 2 : Build

--- src/main/java/org/salim/tutos/App.java
+++ src/main/java/org/salim/tutos/App.java
@@ -1,13 +1,26 @@
 package org.salim.tutos;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        List<Object> keys = java.lang.System.getProperties().keySet().stream().sorted(Comparator.comparing(Object::toString)).collect(Collectors.toList());
+        Iterator<Object> iterKeys = keys.iterator();
+
+        Object o;
+
+        for (int i = 0; i < keys.size(); i++) {
+            o = iterKeys.next();
+            System.out.println(o + " -- " + java.lang.System.getProperties().get(o) + "\n");
+        }
+
     }
 }
